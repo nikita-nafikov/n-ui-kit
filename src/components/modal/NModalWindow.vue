f6482ec189f4335b7272953e35735b954c4f347b
nikita-nafikov <nafikov.nik@mail.ru>
2024-04-12 16:18:07 +0500

    добавил телепорт к body

--- src/components/modal/NModalWindow.vue
+++ src/components/modal/NModalWindow.vue
@@ -1,49 +1,51 @@
 <template>
-  <Transition name="slide-fade">
-    <div
-      v-if="modelValue"
-      class="modal-overlay"
-      @click.stop="handleClose"
-      :class="{ dark: isDarkTheme }"
-    >
-      <div class="modal" @click.stop v-bind="$attrs">
-        <div class="modal-wrapper">
-          <header class="modal-header">
-            <slot name="header">
-              <div class="header__wrapper">
-                <span
-                  class="header__icon-wrapper"
-                  @click.stop="handleClose"
-                  tabindex="0"
-                >
-                  <svg
-                    width="20px"
-                    height="20px"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+  <Teleport to="body">
+    <Transition name="slide-fade">
+      <div
+        v-if="modelValue"
+        class="modal-overlay"
+        @click.stop="handleClose"
+        :class="{ dark: isDarkTheme }"
+      >
+        <div class="modal" @click.stop v-bind="$attrs">
+          <div class="modal-wrapper">
+            <header class="modal-header">
+              <slot name="header">
+                <div class="header__wrapper">
+                  <span
+                    class="header__icon-wrapper"
+                    @click.stop="handleClose"
+                    tabindex="0"
                   >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                </span>
-              </div>
-            </slot>
-          </header>
-          <div class="modal-content">
-            <slot />
+                    <svg
+                      width="20px"
+                      height="20px"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </span>
+                </div>
+              </slot>
+            </header>
+            <div class="modal-content">
+              <slot />
+            </div>
+            <footer v-if="$slots.footer" class="modal-footer">
+              <slot name="footer" />
+            </footer>
           </div>
-          <footer v-if="$slots.footer" class="modal-footer">
-            <slot name="footer" />
-          </footer>
         </div>
       </div>
-    </div>
-  </Transition>
+    </Transition>
+  </Teleport>
 </template>
 
 <script setup lang="ts">
@@ -96,11 +98,11 @@ onUnmounted(() => {
   right: 0;
   bottom: 0;
   transition: all 0.3s ease;
+  z-index: var(--modal-window-z-index);
 }
 
 .modal {
   position: absolute;
-  z-index: var(--modal-window-z-index);
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
