4cde5f5e84932fa37fad157cb6408072e3ba9655
nikita-nafikov <nafikov.nik@mail.ru>
2024-09-11 12:57:13 +0500

    убрал лишнее

--- storybook-static/types/INotification.d.ts
+++
@@ -1,9 +0,0 @@
-import type { VNode } from '../../vue/dist/vue.esm-bundler.js';
-export interface Notification {
-    id: string;
-    type: 'info' | 'warning' | 'error' | 'success';
-    title: null | string;
-    message: null | string | VNode;
-    autoClose: boolean;
-    duration: number;
-}
